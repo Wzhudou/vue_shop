aa17c1d0a387504794328e50c906f28fcf9b49e9
studyVue <jingwg3@163.com>
2023-08-24 21:44:28 +0800

    完成订单页面

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1061,3 +1061,77 @@ export const addGoodsMockData = {
     attr_vals: 'fffff'
 }],
 };
+
+// 订单列表
+export const orderListMockData = {
+  data: [{
+    order_id: 0,
+    user_id: 133,
+    order_number: 'num1njsis',
+    order_price: 322,
+    order_pay: '1',
+    is_send: '是',
+    trade_no: '',
+    order_fapiao_title: '个人',
+    order_fapiao_company: '',
+    order_fapiao_content: '办公用品',
+    consignee_addr: 'a:7:{s:7',
+    pay_status: '1',
+    create_time: 1508331565,
+    update_time: 1508331565,
+  },{
+    order_id: 1,
+    user_id: 133,
+    order_number: 'cbwchiwhviev',
+    order_price: 40,
+    order_pay: '1',
+    is_send: '否',
+    trade_no: '',
+    order_fapiao_title: '个人',
+    order_fapiao_company: '',
+    order_fapiao_content: '办公用品',
+    consignee_addr: 'a:7:{s:7',
+    pay_status: '0',
+    create_time: 1508331565,
+    update_time: 1508331565,
+  }]
+}
+
+export const kuaidiMockData = {
+  data: [{
+    time: '2018-05-10 09:39:00',
+    ftime: '2018-05-10 09:39:00',
+    context: '已签收，感谢使用顺丰，期待再次为您服务',
+    location: '',
+  },{
+    time: '2018-05-10 08:23:00',
+    ftime: '2018-05-10 08:23:00',
+    context: '[北京市]北京海淀育新小区营业点派送员 顺丰速运 95338正在为您派件',
+    location: '',
+  },{
+    time: '2018-05-10 07:32:00',
+    ftime: '2018-05-10 07:32:00',
+    context: '快件到达 [北京海淀育新小区营业点]',
+    location: '',
+  },{
+    time: '2018-05-10 02:03:00',
+    ftime: '2018-05-10 02:03:00',
+    context: '快件到达 [北京海淀育新小区营业点]',
+    location: '',
+  },{
+    time: '2018-05-09 22:32:00',
+    ftime: '2018-05-09 22:32:00',
+    context: '快件已到达北京海淀区转运中心',
+    location: '',
+  },{
+    time: '2018-05-09 13:03:00',
+    ftime: '2018-05-09 13:03:00',
+    context: '快件已发往北京海淀区转运中心',
+    location: '',
+  },{
+    time: '2018-05-09 07:03:00',
+    ftime: '2018-05-09 07:03:00',
+    context: '天津北辰区***公司取件人: 王**（15757430978）已收件',
+    location: '',
+  }],
+}
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,6 +37,8 @@ import {
     CheckboxGroup,
     Checkbox,
     Upload,
+    Timeline,
+    TimelineItem,
  } 
  from 'element-ui';
 
@@ -75,6 +77,8 @@ Vue.use(Step);
 Vue.use(CheckboxGroup);
 Vue.use(Checkbox);
 Vue.use(Upload);
+Vue.use(Timeline);
+Vue.use(TimelineItem);
 // 全局挂载
 Vue.prototype.$message = Message;
 Vue.prototype.$confirm = MessageBox.confirm;
